21689a5db246b0dc1d4618b7741c8fadb530c191
Nishant <fundootestes@gmail.com>
2018-12-31 14:59:18 +0530

    Update

---
+++ pytest/test_command_line_args.py
@@ -0,0 +1,7 @@
+import pytest
+
+def test_command_line_methodA(TimeSetUp, setUptest):
+    print("Running method A")
+
+def test_command_line_methodB(TimeSetUp, sesetUptesttUp):
+    print("Running method B")
